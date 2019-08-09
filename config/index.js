3f65db8e736f03a9689daf2930c12407d01a7dd5
Anton Izhykov <antonizhykov@gmail.com>
2019-08-09 16:29:10 +0300

    Add docker

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 const config = {
-  database: 'mongodb://localhost:27017/my-trello-db',
+  database: `mongodb://${process.env.REACT_APP_DB_URL}:27017/my-trello-db`,
   host: 'localhost',
   port: 5000,
   authentication: {
-    secret: 'nu489c0ACMACIDc0icjiiw'
+    secret: `${process.env.REACT_APP_AUTH_SECRET}`
   }
 };
 
--- controllers/project.js
+++ controllers/project.js
@@ -10,6 +10,7 @@ const getAllProjects = async (req, res) => {
       res.status(404).send({ message: "You haven't created any projects yet." });
     res.status(200).send({ userProjects });
   } catch (error) {
+    console.log(error);
     res.status(500).send({ error });
   }
 };
